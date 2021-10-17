92818dba40aee8014e3b532a1d8fde27a656e4b3
Mikko Koskenranta <mikko.koskenranta@hotmail.com>
2021-10-17 20:13:56 +0300

    Add a stream implementation

---
+++ src/CsvStream.ts
@@ -0,0 +1,88 @@
+import {Readable as ReadableStream} from "stream";
+
+import type {GenerateOptions} from "./types";
+import {HeaderStyle} from "./types";
+import {convertToCell} from "./utility";
+
+/**
+ * Readable stream to convert a set of data arrays into a csv document.
+ */
+export class CsvStream extends ReadableStream{
+    /** The source data arrays to convert into csv. */
+    private readonly data: unknown[][];
+    /** Full options for csv document generation. */
+    private readonly options: GenerateOptions;
+
+    /** The current line of the csv document being processed. */
+    private currentLine = 0;
+    /** A custom header row to write to the start of the csv document. */
+    private customHeader: string[]|null = null;
+
+    /**
+     * Create a stream to convert a set of data arrays into a csv document.
+     * @param data The arrays of data to convert into a csv document.
+     * @param options Optional csv conversion options.
+     */
+    public constructor(data: unknown[][], options: Partial<GenerateOptions> = {}){
+        super();
+        // Check that all data arrays have the same length
+        const targetLength = data[0].length;
+        for(const dataArray of data){
+            if(dataArray.length !== targetLength){
+                throw new Error("All data arrays must have the same length.");
+            }
+        }
+
+        this.data = data;
+        this.options = {
+            cellSeparator: ",",
+            rowSeparator: "\n",
+            headerStyle: HeaderStyle.FIRST_ROW,
+            customHeader: null,
+            ...options
+        };
+
+        // Validate and set the custom header row if one is set in options
+        if(this.options.headerStyle === HeaderStyle.CUSTOM){
+            if(!Array.isArray(this.options.customHeader) || this.options.customHeader.length !== this.data.length){
+                throw new Error("Custom header array must have a header string for each row.");
+            }
+            this.customHeader = this.options.customHeader;
+        }
+    }
+
+    /** @override */
+    public _read(): void{
+        // Write the custom header first if one exists
+        if(this.customHeader !== null){
+            const header: string[] = this.customHeader;
+            this.customHeader = null;
+            if(!this.push(Buffer.from(
+                header.join(this.options.cellSeparator)
+                + this.options.rowSeparator,
+                "utf8"
+            ))){
+                return;
+            }
+        }
+
+        let pushMore: boolean;
+        do{
+            pushMore = this.push(Buffer.from(
+                this.data
+                    .map((dataArray) => {
+                        return convertToCell(dataArray[this.currentLine]);
+                    })
+                    .join(this.options.cellSeparator)
+                + this.options.rowSeparator,
+                "utf8"
+            ))
+            this.currentLine += 1;
+
+            if(this.currentLine >= this.data[0].length){
+                this.push(null);
+                break;
+            }
+        }while(pushMore);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import {Readable as ReadableStream} from "stream";
+
 import type {GenerateOptions} from "./types";
 import {HeaderStyle} from "./types";
 import {convertToCell} from "./utility";
+import {CsvStream} from "./CsvStream";
 
 /**
  * Generate csv document from a set of data arrays.
@@ -9,44 +12,17 @@ import {convertToCell} from "./utility";
  * @returns The generated csv document as a string.
  */
 export function generate(data: unknown[][], options: Partial<GenerateOptions> = {}): string{
+    const stream = new CsvStream(data, options);
     let csv = "";
-    const finalOptions: GenerateOptions = {
-        cellSeparator: ",",
-        rowSeparator: "\n",
-        headerStyle: HeaderStyle.FIRST_ROW,
-        customHeader: null,
-        ...options
-    };
 
-    // Check that all data arrays have the same length
-    const targetLength = data[0].length;
-    for(const dataArray of data){
-        if(dataArray.length !== targetLength){
-            throw new Error("All data arrays must have the same length.");
-        }
+    let row: Buffer|null = Buffer.alloc(0);
+    while(row !== null){
+        csv += row.toString("utf8");
+        row = stream.read();
     }
 
-    // Generate the header row if a non-standard one is set in options
-    if(finalOptions.headerStyle === HeaderStyle.CUSTOM){
-        if(!Array.isArray(finalOptions.customHeader) || finalOptions.customHeader.length !== data.length){
-            throw new Error("Custom header array must have a header string for each row.");
-        }
-        csv += finalOptions.customHeader.join(finalOptions.cellSeparator);
-        csv += finalOptions.rowSeparator;
-    }
-
-    // Generate all data rows
-    for(let i = 0; i < data[0].length; ++i){
-        csv += data
-            .map((dataArray) => {
-                return convertToCell(dataArray[i]);
-            })
-            .join(finalOptions.cellSeparator);
-        csv += finalOptions.rowSeparator;
-    }
-
-    // Don't return the last row change
-    return csv.substring(0, csv.length - finalOptions.rowSeparator.length);
+    return csv;
 }
 
 export {GenerateOptions, HeaderStyle} from "./types";
+export {CsvStream} from "./CsvStream";
