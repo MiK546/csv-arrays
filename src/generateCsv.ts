08e55a388354c65480ad2fd34bca0172d55b6a15
Mikko Koskenranta <mikko.koskenranta@hotmail.com>
2021-10-17 22:00:11 +0300

    Refactor generate csv function

---
+++ src/generateCsv.ts
@@ -0,0 +1,21 @@
+import type {GenerateOptions} from "./types";
+import {CsvStream} from "./CsvStream";
+
+/**
+ * Generate csv document from a set of data arrays.
+ * @param data The arrays of data to convert into a csv document.
+ * @param options Optional csv conversion options.
+ * @returns The generated csv document as a string.
+ */
+export function generateCsv(data: unknown[][], options: Partial<GenerateOptions> = {}): string{
+    const stream = new CsvStream(data, options);
+    let csv = "";
+
+    let row: Buffer|null = Buffer.alloc(0);
+    while(row !== null){
+        csv += row.toString("utf8");
+        row = <Buffer|null>stream.read();
+    }
+
+    return csv;
+}
